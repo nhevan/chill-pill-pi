ea11f3052a7a2d581826d7ed2a72d6c371ed9717
NH Evan <nhevan@gmail.com>
2017-03-08 16:43:38 +0600

    clean index.js. Pusher is perfectly working.

--- index.js
+++ index.js
@@ -3,20 +3,11 @@ var Pusher = require('pusher-js/node');
 var app = express();
 var port = 3700;
 
-app.get("/", function(req, res){
-
-
-    res.send("Hello world");
+var pusher = new Pusher('c6c89850cbcdfffb572e', {
+  cluster: 'ap1',
+  encrypted: true
 });
-
-// app.listen(port);
-
-	var pusher = new Pusher('c6c89850cbcdfffb572e', {
-      cluster: 'ap1',
-      encrypted: true
-    });
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      console.log(data);
-    });
-console.log("Listening on port " + port);
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+  console.log(data);
+});
