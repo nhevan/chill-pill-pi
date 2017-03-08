a7822958dfe9de0520fee06727d183319060c149
NH Evan <nhevan@gmail.com>
2017-03-08 20:29:56 +0600

    simple refactoring

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ var express = require("express");
 var Pusher = require('pusher-js/node');
 var app = express();
 var port = 3700;
+var channel_name = 'my-channel';
+var event_name = 'my-event';
+var pusher_app_key = 'c6c89850cbcdfffb572e';
 
-var pusher = new Pusher('c6c89850cbcdfffb572e', {
-  cluster: 'ap1',
-  encrypted: true
-});
-var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function(data) {
-  console.log(data);
-});
+startListeningToPusher();
+
+function startListeningToPusher(){
+	var pusher = preparePusher();
+	subscribeToChannel(pusher);
+}
+
+function preparePusher(){
+	console.log('preparing ...');
+	var pusher = new Pusher(pusher_app_key, {
+		cluster: 'ap1',
+		encrypted: true
+	});
+	return pusher;
+}
+
+function subscribeToChannel(pusher){
+	var channel = pusher.subscribe(channel_name);
+	console.log('ready ...');
+	channel.bind(event_name, function(data) {
+		processIncomingPush(data);
+	});
+}
+
+function processIncomingPush(data){
+	console.log(data);
+}
