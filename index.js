aeb9e6b994fc02b8a6c1b0d799656afa512197fa
NH Evan <nhevan@gmail.com>
2017-03-08 16:43:05 +0600

    run npm install pusher-js and update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require("express");
+var Pusher = require('pusher-js/node');
 var app = express();
 var port = 3700;
 
 app.get("/", function(req, res){
+
+
     res.send("Hello world");
 });
 
-app.listen(port);
+// app.listen(port);
+
+	var pusher = new Pusher('c6c89850cbcdfffb572e', {
+      cluster: 'ap1',
+      encrypted: true
+    });
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      console.log(data);
+    });
 console.log("Listening on port " + port);
