ecee42ddc4e8b17a101d7788dcc9958050d8130c
NH Evan <nhevan@gmail.com>
2017-03-08 21:25:37 +0600

    use node-schedule to test a scheduled task.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var Pusher = require('pusher-js/node');
+var schedule = require('node-schedule');
 var app = express();
-var port = 3700;
+
 var channel_name = 'my-channel';
 var event_name = 'my-event';
 var pusher_app_key = 'c6c89850cbcdfffb572e';
 
 startListeningToPusher();
+setupAlarms();
 
 function startListeningToPusher(){
 	var pusher = preparePusher();
@@ -24,7 +26,7 @@ function preparePusher(){
 
 function subscribeToChannel(pusher){
 	var channel = pusher.subscribe(channel_name);
-	console.log('ready ...');
+	console.log('listening for updates from server ...');
 	channel.bind(event_name, function(data) {
 		processIncomingPush(data);
 	});
@@ -32,4 +34,14 @@ function subscribeToChannel(pusher){
 
 function processIncomingPush(data){
 	console.log(data);
+}
+
+function setupAlarms(){
+	console.log('preparing for alarm ...');
+
+	var before_breakfast = '0 30 8 * * *'; //everyday at 8:30am
+	schedule.scheduleJob(before_breakfast, function(){
+	  console.log('8:30pm');
+	});	
+
 }
