f0830af14a4b5b28c6314ef8dffa08587b12ebb3
NH Evan <nhevan@gmail.com>
2017-05-29 22:42:20 +0600

    remove alarms for now

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var pi3_pusher = require('./pusherFunctions');
-var alarms = require('./alarms');
+// var alarms = require('./alarms');
 
 var app = express();
 
 pi3_pusher.startListeningToPusher();
-alarms.setupAlarms();
+// alarms.setupAlarms();
 
 
